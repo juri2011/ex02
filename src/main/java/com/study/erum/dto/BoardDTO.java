f847f77b65d11fc5c9bbb4db4afaa46f019be840
juri <ojl98@naver.com>
2024-03-07 11:42:55 +0900

    BoardDTO에 주석 추가

--- src/main/java/com/study/erum/dto/BoardDTO.java
+++ src/main/java/com/study/erum/dto/BoardDTO.java
@@ -1,5 +1,32 @@
 package com.study.erum.dto;
 
-public class BoardDTO {
+import java.sql.Timestamp;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+/*
+  board_table 구조
+    id  bigint
+    boardWriter varchar(50)
+    boardPass varchar(20)
+    boardTitle  varchar(50)
+    boardContents varchar(500)
+    boardCreatedTime  datetime
+    boardHits int
+    fileAttached  int
+*/
+public class BoardDTO {
+  private Long id;
+  private String boardWriter;
+  private String boardPass;
+  private String boardTitle;
+  private String boardContents;
+  private Timestamp boardCreatedTime;
+  private int boardHits;
 }
