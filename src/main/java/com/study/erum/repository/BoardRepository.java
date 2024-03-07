2e986080acad3a3fab9e5b70e6afb0ca8e20f657
juri <ojl98@naver.com>
2024-03-07 14:07:34 +0900

    boardMapper에 updateHits 추가하고 index.jsp a태그 수정

--- src/main/java/com/study/erum/repository/BoardRepository.java
+++ src/main/java/com/study/erum/repository/BoardRepository.java
@@ -27,4 +27,8 @@ public class BoardRepository {
     return sql.selectOne("Board.findById", id);
   }
 
+  public void updateHits(Long id) {
+    sql.update("Board.updateHits", id);
+  }
+
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -10,6 +10,6 @@
 <body>
 	<h2>Hello Springframework</h2>
 	<a href="/board/save">글작성</a>
-	<a href="/board">글목록</a>
+	<a href="/board/">글목록</a>
 </body>
 </html>
