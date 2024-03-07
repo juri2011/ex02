c4a455b349afff48cdf84ecf627d55e2b9d2e1e9
juri <ojl98@naver.com>
2024-03-07 11:26:08 +0900

    HomeController의 내용을 변경

--- src/main/java/com/study/erum/HomeController.java
+++ src/main/java/com/study/erum/HomeController.java
@@ -1,39 +1,30 @@
 package com.study.erum;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.log4j.Log4j;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
+@Log4j
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-	
+  /*
+  RequestMapping({,})
+  -> 중괄호 : 두 개 이상의 경로가 들어갈 때
+  
+  ex01 프로젝트에선 /member/와 /member 가 달랐는데
+  여기서는 /index/와 /index가 동일하게 적용될 수 있도록 두 개의 경로를 적기 위해 중괄호를 활용하는 것이다.
+  */
+  @RequestMapping({"","/"})
+  public String home() {
+   //log4j의 info 레벨에서 표시(warning 레벨에선 표시되지 않음)
+   //참고로 개발 할 땐 info, 개발이 끝날 땐 warning으로 레벨을 바꾼다.
+   log.info("Welcome home!");
+   return "index";
+  }
+  	
 }
